8756a130489f0d76da5600d6fa990ee98ca665b9
Marcantoine4581 <marcantoine.vannier@gmail.com>
2023-04-03 10:05:13 +0000

    starts a Flask web application

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 """Starts a Flask web application"""
 from flask import Flask
-
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def index():
     """returns Hello HBNB!"""
-    return 'Hello HBNB!'
+    return "Hello HBNB!"
 
 
 if __name__ == '__main__':
