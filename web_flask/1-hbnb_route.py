3f4ec974ac68f9529ca794d35b75915d6bffe73f
Jean-Luc <jlucbilo@gmail.com>
2023-12-13 09:05:03 +0100

    a script that starts a Flask web application and displays twis from different url

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,20 +1,22 @@
 #!/usr/bin/python3
-"""Starts a Flask web application"""
+
+"""Starting of Flask web application"""
 from flask import Flask
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def index():
-    """returns Hello HBNB!"""
+    """Diplayin on to the web page 'Hello HBNB!'"""
     return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """returns HBNB"""
+    """Displaing on to the web page 'HBNB'"""
     return "HBNB"
 
 
 if __name__ == '__main__':
+	""" Making the the web page reachable on the local network """
     app.run(host='0.0.0.0', port='5000')
