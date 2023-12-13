fbe5aed00409550ff8a728c85aa4236c988f52a0
Jean-Luc <jlucbilo@gmail.com>
2023-12-13 09:52:33 +0100

    fixing indentations errors

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -7,14 +7,14 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def index():
-    """Diplayin on to the web page 'Hello HBNB!'"""
-    return "Hello HBNB!"
+	"""Diplayin on to the web page 'Hello HBNB!'"""
+	return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """Displaing on to the web page 'HBNB'"""
-    return "HBNB"
+	"""Displaing on to the web page 'HBNB'"""
+	return "HBNB"
 
 
 if __name__ == '__main__':
